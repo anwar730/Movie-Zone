427db38357c2a337bb57203fc9f19a56e8e0af7c
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-08-30 21:49:37 +0300

    Fetch the trending movies from the API and render it to the page

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,47 @@
 
+// function fetchTrending() {
+// fetch('https://api.themoviedb.org/3/trending/all/day?api_key=d3b69953f79121feef98281a3e589d66')
+// .then(res => res.json())
+// .then (data => {
+//     getTrending(data.results)
+// })
+// }
+// fetchTrending()
+
+// const getTrending = (movies) => {
+//     movies.forEach(movie => {
+//         const p=document.querySelector('#movieName')
+//         p.textContent=`The movie name is ${movie.title}`
+//         console.log(movie.title)
+//     });
+// }
+function getAllTrendingMovies(){
+    fetch('https://api.themoviedb.org/3/trending/all/day?api_key=d3b69953f79121feef98281a3e589d66')
+    .then(response=>(response.json()))
+    // .then (data=>console.log(data.results))
+    .then(data=>{
+        renderOneMovie(data.results)
+    })
+}
+
+function renderOneMovie(movies){
+    movies.forEach(movie => {
+    console.log(movie)
+
+    let box=document.createElement('li')
+    box.className="trend-list"
+    box.innerHTML=`
+    <img id="trend-imgs"src ="https://image.tmdb.org/t/p/w500${movie.backdrop_path}">
+    <h4>${movie.title || movie.name}</h4>
+    <p>Release date: ${movie.release_date || movie.first_air_date}</p>
+    <p>Vote Count:${movie.vote_count}</p>
+    `
+    document.querySelector('.tr-list').append(box)
+    });
+}
+
+function initialize(){
+    getAllTrendingMovies()
+
+}
+initialize()
